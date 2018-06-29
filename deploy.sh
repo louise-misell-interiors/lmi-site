7fa74378ca717eebd4753f41126416f97ebb7bdc
Benjamin Misell <benjamin@misell.cymru>
2018-06-29 14:58:46 +0100

    Add wait for deploy to finish

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,4 @@ HASH=`git log --pretty=format:'%H' -n 1`
 docker build -t evilben/lmi-django:$HASH .
 docker push evilben/lmi-django:$HASH
 cat kubes/django.yml | sed "s/(hash)/$HASH/g" | kubectl apply -f -
+kubectl -n lmi rollout status deployment/site-django

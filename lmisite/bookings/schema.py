1e8025c9da72121a677a7f624c8cb61c743b0aaa
Benjamin Misell <benjamin@misell.cymru>
2018-07-05 13:23:14 +0100

    Add to calendar

--- lmisite/bookings/schema.py
+++ lmisite/bookings/schema.py
@@ -12,103 +12,155 @@ import dateutil.parser
 from .views import API_SERVICE_NAME, API_VERSION, get_credentials
 
 
-def get_calendar_events(date: datetime.date):
+def get_calendar_events(start_date: datetime.date, end_date: datetime.date):
     creds = get_credentials()
-    calendar = googleapiclient.discovery.build(
-        API_SERVICE_NAME, API_VERSION, credentials=creds)
+    out = {}
+    date = start_date
+    while date <= end_date:
+        out[date] = []
+        date += datetime.timedelta(days=1)
+    if creds is not None:
+        calendar = googleapiclient.discovery.build(
+            API_SERVICE_NAME, API_VERSION, credentials=creds)
 
-    events = calendar.events()\
-        .list(calendarId="primary", orderBy='startTime', singleEvents=True,
-              timeMin=datetime.datetime.combine(date, datetime.time.min).isoformat() + 'Z',
-              timeMax=datetime.datetime.combine(date, datetime.time.max).isoformat() + 'Z')\
-        .execute()
+        events = calendar.events()\
+            .list(calendarId="primary", orderBy='startTime', singleEvents=True,
+                  timeMin=datetime.datetime.combine(start_date, datetime.time.min).isoformat() + 'Z',
+                  timeMax=datetime.datetime.combine(end_date, datetime.time.max).isoformat() + 'Z')\
+            .execute()
 
-    return list(filter(lambda event: event.get("status", "") == 'confirmed' and event.get('kind', '') == 'calendar#event', events.get('items', [])))
+        events = filter(lambda e: e.get("status", "") == 'confirmed' and e.get('kind', '') == 'calendar#event', events.get('items', []))
+        for event in events:
+            start = event['start'].get('dateTime')
+            if start:
+                start = timezone.make_naive(dateutil.parser.parse(start)).date()
+                out[start].append(event)
+
+    return out
 
 
-def get_booking_times(date: datetime.date, booking: models.BookingType):
+def insert_booking_to_calendar(booking: models.Booking):
+    creds = get_credentials()
+    if creds is not None:
+        calendar = googleapiclient.discovery.build(
+            API_SERVICE_NAME, API_VERSION, credentials=creds)
+
+        calendar.events().insert(calendarId="primary", body={
+            "start": {
+                "dateTime": booking.time.isoformat(),
+                "timeZone": booking.time.tzname()
+            },
+            "end": {
+                "dateTime": (booking.time + booking.type.length).isoformat(),
+                "timeZone": (booking.time + booking.type.length).tzname()
+            },
+            "summary": str(booking),
+            "attendees": [
+                {
+                    "displayName": booking.customer.name,
+                    "responseStatus": "accepted",
+                    "email": booking.customer.email,
+                    "comment": f"Phone: {booking.customer.phone}"
+                }
+            ],
+        }, sendNotifications=True).execute()
+
+
+def get_booking_times(start_date: datetime.date, booking: models.BookingType, end_date=None):
+    if end_date is None:
+        end_date = start_date
+
     tz = pytz.timezone(booking.timezone)
-
-    times = []
-    cur_time = datetime.datetime.combine(date, datetime.datetime.min.time())
-
-    now = datetime.datetime.now()
+    cal_events = get_calendar_events(start_date, end_date)
     rules = booking.booking_rules.all()
-    bookings_on_day = models.Booking.objects.filter(time__date=cur_time.date())
-    cal_events = get_calendar_events(date)
-    while cur_time.time() <= datetime.time(23):
-        valid = True
+    now = datetime.datetime.now()
 
-        if cur_time < now:
-            valid = False
-        if (now + booking.minimum_scheduling_notice) > cur_time:
-            valid = False
+    out = {}
 
-        passes_rules = False
-        if valid:
-            for rule in rules:
-                if rule.start_date > cur_time.date():
-                    continue
-                if not rule.recurring and rule.end_date < cur_time.date():
-                    continue
+    date = start_date
+    while date <= end_date:
+        times = []
+        cur_time = datetime.datetime.combine(date, datetime.datetime.min.time())
 
-                if not rule.monday and cur_time.weekday() == 0:
-                    continue
-                if not rule.tuesday and cur_time.weekday() == 1:
-                    continue
-                if not rule.wednesday and cur_time.weekday() == 2:
-                    continue
-                if not rule.thursday and cur_time.weekday() == 3:
-                    continue
-                if not rule.friday and cur_time.weekday() == 4:
-                    continue
-                if not rule.saturday and cur_time.weekday() == 5:
-                    continue
-                if not rule.sunday and cur_time.weekday() == 6:
-                    continue
+        bookings_on_day = models.Booking.objects.filter(time__date=cur_time.date())
+        while cur_time.time() <= datetime.time(23):
+            valid = True
 
-                start_time = timezone.make_aware(
-                    datetime.datetime.combine(datetime.datetime(1970, 1, 1), rule.start_time), tz)\
-                    .astimezone(pytz.utc).time()
-                end_time = timezone.make_aware(
-                    datetime.datetime.combine(datetime.datetime(1970, 1, 1), rule.end_time), tz)\
-                    .astimezone(pytz.utc).time()
-                if not (start_time <= cur_time.time() and end_time >= (cur_time + booking.length).time()):
-                    continue
-
-                passes_rules = True
-        if not passes_rules:
-            valid = False
-
-        if valid:
-            if booking.max_events_per_day is not None and len(bookings_on_day) >= booking.max_events_per_day:
+            if cur_time < now:
                 valid = False
-            else:
-                for b in bookings_on_day:
-                    time = timezone.make_naive(b.time)
-                    if time <= cur_time < (
-                            time + b.type.length + max(booking.buffer_before_event, b.type.buffer_after_event)):
-                        valid = False
-                    if time < (
-                            cur_time + booking.length + max(booking.buffer_after_event, b.type.buffer_before_event)) < (
-                            time + b.type.length):
-                        valid = False
-            for b in cal_events:
-                start = b['start'].get('dateTime')
-                end = b['end'].get('dateTime')
-                if start and end:
-                    start = timezone.make_naive(dateutil.parser.parse(start))
-                    end = timezone.make_naive(dateutil.parser.parse(end))
-                    if start <= cur_time < (end + booking.buffer_before_event):
-                        valid = False
-                    if start < (cur_time + booking.length + booking.buffer_after_event) < end:
-                        valid = False
+            if (now + booking.minimum_scheduling_notice) > cur_time:
+                valid = False
 
-        if valid:
-            times.append(cur_time.time())
-        cur_time += booking.scheduling_frequency
+            passes_rules = False
+            if valid:
+                for rule in rules:
+                    if rule.start_date > cur_time.date():
+                        continue
+                    if not rule.recurring and rule.end_date < cur_time.date():
+                        continue
 
-    return times
+                    if not rule.monday and cur_time.weekday() == 0:
+                        continue
+                    if not rule.tuesday and cur_time.weekday() == 1:
+                        continue
+                    if not rule.wednesday and cur_time.weekday() == 2:
+                        continue
+                    if not rule.thursday and cur_time.weekday() == 3:
+                        continue
+                    if not rule.friday and cur_time.weekday() == 4:
+                        continue
+                    if not rule.saturday and cur_time.weekday() == 5:
+                        continue
+                    if not rule.sunday and cur_time.weekday() == 6:
+                        continue
+
+                    start_time = timezone.make_aware(
+                        datetime.datetime.combine(datetime.datetime(1970, 1, 1), rule.start_time), tz)\
+                        .astimezone(pytz.utc).time()
+                    end_time = timezone.make_aware(
+                        datetime.datetime.combine(datetime.datetime(1970, 1, 1), rule.end_time), tz)\
+                        .astimezone(pytz.utc).time()
+                    if not (start_time <= cur_time.time() and end_time >= (cur_time + booking.length).time()):
+                        continue
+
+                    passes_rules = True
+            if not passes_rules:
+                valid = False
+
+            if valid:
+                if booking.max_events_per_day is not None and len(bookings_on_day) >= booking.max_events_per_day:
+                    valid = False
+                else:
+                    for b in bookings_on_day:
+                        time = timezone.make_naive(b.time)
+                        if time <= cur_time < (
+                                time + b.type.length + max(booking.buffer_before_event, b.type.buffer_after_event)):
+                            valid = False
+                        if time < (
+                                cur_time + booking.length + max(booking.buffer_after_event, b.type.buffer_before_event)) < (
+                                time + b.type.length):
+                            valid = False
+
+            if valid:
+                for b in cal_events[date]:
+                    start = b['start'].get('dateTime')
+                    end = b['end'].get('dateTime')
+                    if start and end:
+                        start = timezone.make_naive(dateutil.parser.parse(start))
+                        end = timezone.make_naive(dateutil.parser.parse(end))
+                        if start <= cur_time < (end + booking.buffer_before_event):
+                            valid = False
+                        if start < (cur_time + booking.length + booking.buffer_after_event) < end:
+                            valid = False
+
+            if valid:
+                times.append(cur_time.time())
+            cur_time += booking.scheduling_frequency
+
+        out[date] = times
+        date += datetime.timedelta(days=1)
+
+    return out
 
 
 class BookingQuestion(DjangoObjectType):
@@ -144,20 +196,25 @@ class BookingType(DjangoObjectType):
         return self.booking_questions.all()
 
     def resolve_booking_times(self, info, date):
-        return get_booking_times(date, self)
+        return get_booking_times(date, self)[date]
 
     def resolve_booking_days(self, info, start, num):
         days = []
         day = start
         num_tried = 0
+
+        def get_days(start):
+            return get_booking_times(start, self, start + datetime.timedelta(days=num))
+
         while len(days) < num:
-            if len(get_booking_times(day, self)) > 0:
-                days.append(day)
-            else:
-                num_tried += 1
-                if num_tried >= 60:
-                    break
-            day += datetime.timedelta(days=1)
+            new_days = get_days(day)
+            for d, t in new_days.items():
+                print(d, t)
+                if len(t) > 0:
+                    days.append(d)
+            num_tried += len(new_days)
+            if num_tried >= 20:
+                break
 
         return days
 
@@ -188,7 +245,7 @@ class CreateBooking(graphene.Mutation):
     def mutate(self, info, id, date, time, name, email, phone, questions):
         booking_type = models.BookingType.objects.get(pk=id)
         tz = pytz.timezone(booking_type.timezone)
-        booking_times = get_booking_times(date, booking_type)
+        booking_times = get_booking_times(date, booking_type)[date]
 
         if time not in booking_times:
             return CreateBooking(ok=False, error=["Unavailable booking time"])
@@ -210,7 +267,8 @@ class CreateBooking(graphene.Mutation):
             return CreateBooking(ok=False, error=map(lambda error: error[0].title() + ": " + error[1][0], e))
         customer.save()
 
-        booking.time = timezone.make_aware(datetime.datetime.combine(date, time), tz)
+        time = timezone.make_aware(datetime.datetime.combine(date, time), pytz.utc)
+        booking.time = time
         booking.type = booking_type
         booking.customer = customer
 
@@ -218,15 +276,19 @@ class CreateBooking(graphene.Mutation):
             booking.full_clean()
         except ValidationError as e:
             return CreateBooking(ok=False, error=map(lambda error: error[0].title() + ": " + error[1][0], e))
-        booking.save()
+        # booking.save()
 
         subject = f"{name} has booked {booking_type.name}"
-        time = datetime.datetime.combine(date, time).strftime("%I:%M%p %a %d %b %Y")
-        body = f"Name: {name}\r\nEmail: {email}\r\nPhone: {phone}\r\n\r\n---\r\n\r\n{booking_type.name}\r\nTime: {time}"
+        tz = pytz.timezone(booking_type.timezone)
+        time = time.astimezone(tz=tz).strftime("%I:%M%p %a %d %b %Y")
+        body = f"Name: {name}\r\nEmail: {email}\r\nPhone: {phone}\r\n\r\n---\r\n\r\n{booking_type.name}\r\n" \
+               f"Time: {time}, {booking_type.timezone}"
 
         config = SiteConfig.objects.first()
         recipients = [config.email]
         send_mail(subject, body, email, recipients)
+
+        insert_booking_to_calendar(booking)
 
         return CreateBooking(ok=True, error="")
 

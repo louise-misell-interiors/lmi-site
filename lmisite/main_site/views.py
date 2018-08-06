28869bc6c3badffeeae9249ca3158104759cbb88
Benjamin Misell <benjamin@misell.cymru>
2018-08-06 18:25:58 +0100

    Display drafts to superusers

--- lmisite/main_site/views.py
+++ lmisite/main_site/views.py
@@ -8,9 +8,12 @@ import bookings.models as booking_models
 
 def index(request):
     slider_imgs = MainSliderImage.objects.all()
-    testimonials = Testimonial.objects.filter(featured=True, draft=False)
+    testimonials = Testimonial.objects.filter(featured=True)
     projects = Project.objects.filter(draft=False)[:4]
-    services = Service.objects.filter(type=Service.MAIN, draft=False)
+    services = Service.objects.filter(type=Service.MAIN)
+    if not request.user.is_superuser:
+        testimonials = testimonials.filter(draft=False)
+        services = services.filter(draft=False)
     return render(request, "main_site/index.html",
                   {"testimonials": testimonials, "slider_imgs": slider_imgs, "projects": projects,
                    "services": services})
@@ -21,7 +24,9 @@ def config(request):
 
 
 def design_insider(request):
-    posts = DesignInsiderPost.objects.filter(draft=False)
+    posts = DesignInsiderPost.objects.all()
+    if not request.user.is_superuser:
+        posts = posts.filter(draft=False)
 
     posts1 = posts[0::3]
     posts2 = posts[1::3]
@@ -30,34 +35,49 @@ def design_insider(request):
 
 
 def design_insider_post(request, id):
-    post = get_object_or_404(DesignInsiderPost, id=id, draft=False)
+    if not request.user.is_superuser:
+        post = get_object_or_404(DesignInsiderPost, id=id, draft=False)
+    else:
+        post = get_object_or_404(DesignInsiderPost, id=id)
     return render(request, "main_site/design_insider_post.html", {"post": post})
 
 
 def about(request):
-    sections = AboutSection.objects.filter(draft=False)
+    sections = AboutSection.objects.all()
+    if not request.user.is_superuser:
+        sections = sections.filter(draft=False)
     return render(request, "main_site/about.html", {"sections": sections})
 
 
 def portfolio(request):
-    projects = Project.objects.filter(draft=False)
+    projects = Project.objects.all()
+    if not request.user.is_superuser:
+        projects = projects.filter(draft=False)
     return render(request, "main_site/portfolio.html", {"projects": projects})
 
 
 def project(request, id):
-    project = get_object_or_404(Project, id=id, draft=False)
+    if not request.user.is_superuser:
+        project = get_object_or_404(Project, id=id, draft=False)
+    else:
+        project = get_object_or_404(Project, id=id)
     return render(request, "main_site/project.html", {"project": project})
 
 
 def services(request):
-    services_m = Service.objects.filter(type=Service.MAIN, draft=False)
-    services_o = Service.objects.filter(type=Service.OTHER, draft=False)
+    services_m = Service.objects.filter(type=Service.MAIN)
+    services_o = Service.objects.filter(type=Service.OTHER)
+    if not request.user.is_superuser:
+        services_m = services_m.filter(draft=False)
+        services_o = services_o.filter(draft=False)
     services = list(itertools.zip_longest(services_m, services_o))
     return render(request, "main_site/services.html", {"services": services})
 
 
 def testimonials(request):
-    testimonials = Testimonial.objects.filter(not_on_testimonials=False, draft=False)
+    testimonials = Testimonial.objects.filter(not_on_testimonials=False)
+    if not request.user.is_superuser:
+        testimonials = testimonials.filter(draft=False)
     return render(request, "main_site/testimonials.html", {"testimonials": testimonials})
 
 

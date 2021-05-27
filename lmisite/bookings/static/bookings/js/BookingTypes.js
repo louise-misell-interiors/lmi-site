9102f8abd863d1d5b1cbdb4f4650023ea66a5d5c
Q <q@misell.cymru>
2021-05-27 18:04:29 +0100

    fix booking take 3

--- lmisite/bookings/static/bookings/js/BookingTypes.js
+++ lmisite/bookings/static/bookings/js/BookingTypes.js
@@ -44,7 +44,7 @@ export class BookingTypes extends Component {
             }`)
             .then(res => self.setState({
                 types: res.data.bookingTypes,
-                notice: res.data.config.bookingNotice,
+                notice: res.data.bookingConfig.bookingNotice,
                 loading: false,
             }))
             .catch(err => this.setState({
